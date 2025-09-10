e706572c8f35527122e7e935b3ad0c8758339bc9
YuriTayner <yuritayner3@gmail.com>
2025-09-10 17:40:10 -0300

    Aula do dia 10/09

---
+++ Spring/telas/src/main/resources/static/js/script.js
@@ -0,0 +1,17 @@
+const cpfMask = document.querySelector('#cpf');
+const telMask = document.querySelector('#telefone');
+
+cpfMask.addEventListener('input', function(){
+    this.value = this.value
+    .replace(/\D/g, '')
+    .replace(/(\d{3})(\d)/, '$1.$2')
+    .replace(/(\d{3})(\d)/, '$1.$2')
+    .replace(/(\d{3})(\d{1,2})$/, '$1-$2')
+});
+
+telMask.addEventListener('input', function(){
+    this.value = this.value.replace(/\D/g, '')
+    .replace(/(\d{2})(\d)/, '($1) $2')
+    .replace(/(\d{4,5})(\d)/, '$1-$2')
+    .replace(/(-\d{4})\d+?$/, '$1')
+});
