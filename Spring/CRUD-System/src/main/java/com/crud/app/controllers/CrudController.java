726ed9a033a39e167ed9476051128977c5fd390b
YuriTayner <yuritayner3@gmail.com>
2025-09-17 17:39:28 -0300

    Aula do dia 17/09

--- Spring/CRUD-System/src/main/java/com/crud/app/controllers/CrudController.java
+++ Spring/CRUD-System/src/main/java/com/crud/app/controllers/CrudController.java
@@ -2,12 +2,18 @@ package com.crud.app.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.crud.app.models.Pessoa;
 import com.crud.app.repository.AppRepository;
+
+import ch.qos.logback.core.model.Model;
 
 @Controller
 public class CrudController {
@@ -37,8 +43,36 @@ public class CrudController {
         Iterable<Pessoa> usuarios = csr.findAll();
         mv.addObject("usuarios", usuarios);
         return mv;
+    }
+    
+    @RequestMapping(value="/alterar/{idPessoa}", method=RequestMethod.GET)
+    public ModelAndView alterarUsuario(@PathVariable("idPessoa") long idPessoa){
+        Pessoa usuario = csr.findByIdPessoa(idPessoa);
+        ModelAndView mv = new ModelAndView("alterar");
+        mv.addObject("usuario", usuario);
+        return mv;
+    }
 
-        
+    @RequestMapping(value="/alterar/{idPessoa}", method=RequestMethod.POST)
+    public String alterarUsuario(@Validated Pessoa usuario, BindingResult result, RedirectAttributes attributes){
+        csr.save(usuario);
+        return "redirect:/listar";
+    }
+
+    @RequestMapping("/confirmarExcluir/{idPessoa}")
+    public ModelAndView confirmarExcluir(@PathVariable("idPessoa")
+    long idPessoa){
+        Pessoa usuario = csr.findByIdPessoa(idPessoa);
+        ModelAndView mv = new ModelAndView("ExcluirUsuario");
+        mv.addObject("usuario", usuario);
+        return mv;
+    }
+
+    @RequestMapping("/excluir")
+    public String excluirUsuario(long idPessoa){
+        Pessoa usuario = csr.findByIdPessoa(idPessoa);
+        csr.delete(usuario);
+        return "redirect:/listar";
     }
 }
 
--- Spring/CRUD-System/src/main/java/com/crud/app/repository/AppRepository.java
+++ Spring/CRUD-System/src/main/java/com/crud/app/repository/AppRepository.java
@@ -2,10 +2,10 @@ package com.crud.app.repository;
 
 import org.springframework.data.repository.CrudRepository;
 import com.crud.app.models.Pessoa;
-import java.util.List;
 
 
 
 public interface AppRepository extends CrudRepository<Pessoa, String> {
     Pessoa findByIdPessoa(long idPessoa);
+    Pessoa deleteByIdPessoa(long idPessoa);
 }
