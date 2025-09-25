1269044b6f33a126edaf8d23451cbd910e8fd4ff
YuriTayner <yuritayner3@gmail.com>
2025-09-24 21:33:29 -0300

    Aula do dia 24/09

---
+++ ProjetoFInal/ProjetoFinal/src/main/resources/static/js/formIndex.js
@@ -0,0 +1,7 @@
+ function abrirForm() {
+      document.getElementById("form-overlay").style.display = "grid";
+    }
+
+function fecharForm() {
+      document.getElementById("form-overlay").style.display = "none";
+    }
