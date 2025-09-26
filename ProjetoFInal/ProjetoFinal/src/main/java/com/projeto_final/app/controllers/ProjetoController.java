b4a010e8114f894588267b4a6cb7d84168487c77
YuriTayner <yuritayner3@gmail.com>
2025-09-26 17:31:49 -0300

    Aula do dia 26/09

--- ProjetoFInal/ProjetoFinal/src/main/java/com/projeto_final/app/controllers/ProjetoController.java
+++ ProjetoFInal/ProjetoFinal/src/main/java/com/projeto_final/app/controllers/ProjetoController.java
@@ -2,8 +2,10 @@ package com.projeto_final.app.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.projeto_final.app.models.Usuario;
 import com.projeto_final.app.repository.AppRepository;
@@ -13,24 +15,24 @@ public class ProjetoController {
     @Autowired
     private AppRepository csr;
 
+    //request map do index
     @RequestMapping(value="/", method=RequestMethod.GET)
     public String cadastrarUsuario(){
         return "index";
     }
 
+    //request map do cadastrar usuario
     @RequestMapping(value="/", method=RequestMethod.POST)
     public String cadastrarUsuario(Usuario usuario){
         csr.save(usuario);
-        return "redirect:/indexLogged";
+        return "redirect:/indexLogged/{idUsuario}";
     }
 
-    @RequestMapping("/indexLogged")
-    public String indexLogged(){
-        return "indexLogged";
-    }    
-
-    // @RequestMapping(value="/indexLogged/{idUsuario}", method=RequestMethod.GET)
-    // public ModelAndView indexLogged(){
-    //     return "indexLogged";
-    // }    TERMINAAAAAAAAAAAAAA 
+    @RequestMapping(value="/indexLogged/{idUsuario}", method=RequestMethod.GET)
+    public ModelAndView indexLogged(@PathVariable ("idUsuario") long idUsuario){
+        Usuario usuario = csr.findByIdUsuario(idUsuario);
+        ModelAndView mv = new ModelAndView("indexLogged");
+        mv.addObject("usuario", usuario);
+        return mv;
+    }
 }
